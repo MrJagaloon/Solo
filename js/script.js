ffe4bb583f3159c047c727f7e504d368b0201734
Justin Gifford <justingifford@icloud.com>
2019-04-09 12:33:11 -0500

    Finished team section. Added waypoints jquery plugin.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,40 @@
   Preloader 
 ============================================== */
 $(window).on('load', function () {
+  // fade out preloader
   $('#status').fadeOut('slow');
   $('#preloader').delay(350).fadeOut('slow');
 });
 
 /* ==============================================
-  Content 
+  Team 
 ============================================== */
+$(function () {
+
+  // setup team member carousel
+  $("#team-members").owlCarousel({
+    items: 2,
+    loop: true,
+    smartSpeed: 700,
+    autoplay: true,
+    autoplayHoverPause: true,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+  });
+
+  // animate progress bars on scroll
+  $('#progress-elements').waypoint(function () {
+    $('.progress-bar').each(function () {
+      $(this).animate({
+        width: $(this).attr('aria-valuenow') + '%'
+      }, 1000);
+    });
+    this.destroy();
+  }, {
+    offset: 'bottom-in-view'
+  });
+});
